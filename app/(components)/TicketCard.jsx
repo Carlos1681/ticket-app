7156f8c78d60ca6b33afe28ea97991fb1b88fc23
Carlos1681 <spjcarlosh@gmail.com>
2024-05-08 19:03:16 -0300

    Styling the ticket card

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -15,8 +15,16 @@ const TicketCard = () => {
       <h4>Titudo do Tickets</h4>
       <hr className="h-px border-0 bg-page mb-2" />
       <p className="whitespace-pre-wrap">Descrição do ticket!</p>
-      <ProgressDisplay />
-      <StatusDisplay />
+      <div className="text-grow"></div>
+      <div className="flex mt-2">
+        <div className="flex flex-col">
+          <p className="text-xs my-1">08/05/2024 18:34</p>
+          <ProgressDisplay />
+        </div>
+        <div className="ml-auto flex items-end">
+          <StatusDisplay />
+        </div>
+      </div>
     </div>
   );
 };
